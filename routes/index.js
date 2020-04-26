b19e201a02374409b502fd1a6293ee6f563e3b57
GASHIRA-E <dsteams@outlook.jp>
2020-04-26 17:08:36 +0900

    title周りとか

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+const testUser = {
+  name: 'Ue',
+  idName: 'au',
+  seqUserId: 123123
+};
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: '天気の子を発見',
+    user: testUser,
+    cartItemList
+  });
 });
 
 module.exports = router;
